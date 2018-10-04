6beb870a04665799aa0a69efe5ab02b32ce35c59
Nina Apple <nina.apple@ukr.net>
2018-10-04 23:10:06 +0300

    Hello world

--- AutomationCases/AutomationCases/Program.cs
+++ AutomationCases/AutomationCases/Program.cs
@@ -10,6 +10,8 @@ namespace AutomationCases
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, world");
+            Console.ReadKey();
         }
     }
 }
